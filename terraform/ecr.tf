66406d82a4e40bc47e458bf1e15c0b3dc99ecd4f
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-19 12:43:03 +0200

    Fix

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,7 @@
-resource "aws_ecr_repository" "nginx" {
+data "aws_ecr_repository" "nginx" {
   name = "nginx-hello"
 }
 
-
+output "ecr_repository_url" {
+  value = data.aws_ecr_repository.nginx.repository_url
+}
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.nginx.repository_url
-}
