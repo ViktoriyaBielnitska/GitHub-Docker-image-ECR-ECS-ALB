cb580d523c816d882750758b735ce2bf3c81c6e8
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-19 14:00:52 +0200

    Fix

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -55,7 +55,7 @@ resource "aws_lb" "nginx_alb" {
   name               = "nginx-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = module.vpc.public_subnets
 }
 
