ec8130c680f434520105b2872cc56c8fef2aceed
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-20 00:55:50 +0200

    Fix

--- terraform/ecs_task.tf
+++ terraform/ecs_task.tf
@@ -2,8 +2,8 @@ resource "aws_ecs_task_definition" "nginx" {
   family                   = "nginx-hello"
   requires_compatibilities = ["EC2"]
   network_mode             = "bridge"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "128"
+  memory                   = "256"
 
   container_definitions = jsonencode([
     {
