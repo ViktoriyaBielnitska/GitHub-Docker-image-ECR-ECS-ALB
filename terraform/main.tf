6380d77cb330280134bd9ff309b7894e7d333eaf
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-19 13:51:16 +0200

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,16 +4,22 @@ module "ecs" {
 
   cluster_name = "ecs-ec2"
 }
-# ECS optimized AMI
-data "aws_ami" "ecs" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-ecs-hvm-*"]
-  }
+module "ecs_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.0"
 
+  name = "ecs-asg"
+
+  min_size         = 1
+  max_size         = 1
+  desired_capacity = 1
+
+  vpc_zone_identifier = module.vpc.public_subnets
+  image_id            = data.aws_ami.ecs.id
+  instance_type       = "t3.micro"
+
+  user_data = base64encode("echo ECS_CLUSTER=ecs-ec2 >> /etc/ecs/ecs.config")
 }
 # -------------------
 # ALB (official ALB module)
