7f704bf70eebd305b9f5d20a20a8ce848b074d21
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-21 22:43:33 +0200

    Fix

--- terraform/ecs_asg.tf
+++ terraform/ecs_asg.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
-  role = data.aws_iam_role.ecs_instance_role.name
+  role       = data.aws_iam_role.ecs_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 resource "aws_iam_role" "ecs_task_execution_role" {
