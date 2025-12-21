6fd08f6205380a5582b7885af4ad213c23d6bb28
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-21 22:30:15 +0200

    Fix

--- terraform/ecs_task.tf
+++ terraform/ecs_task.tf
@@ -4,17 +4,18 @@ resource "aws_ecs_task_definition" "nginx" {
   network_mode             = "bridge"
   cpu                      = "128"
   memory                   = "256"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
       name      = "nginx"
-      image     = "${data.aws_ecr_repository.nginx.repository_url}:latest"
+      image     = "386930771365.dkr.ecr.us-east-1.amazonaws.com/nginx-hello:latest"
       essential = true
 
       portMappings = [
         {
           containerPort = 80
-          hostPort      = 80
+          hostPort      = 0
         }
       ]
     }
