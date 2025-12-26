39ee7dfa2e78dff67748e1cc4f5aeda015ebbae2
Вікторія <viktoriyabelnicka@gmail.com>
2025-12-26 12:40:38 +0200

    Fix main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,7 +100,7 @@ resource "aws_lb_target_group" "ecs" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
-  target_type = "instance"
+  target_type = "instance" # для EC2 launch type
 
   health_check {
     path                = "/"
@@ -110,11 +110,13 @@ resource "aws_lb_target_group" "ecs" {
     unhealthy_threshold = 2
     matcher             = "200-399"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lb_listener" "http" {
-  depends_on = [aws_lb_target_group.ecs]
-
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
@@ -123,8 +125,11 @@ resource "aws_lb_listener" "http" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs.arn
   }
-}
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 ############################
 # ECS SERVICE (EC2)
@@ -136,7 +141,7 @@ module "ecs_service" {
   name         = "nginx"
   cluster_arn  = module.ecs.cluster_arn
   launch_type  = "EC2"
-  network_mode = "bridge"
+  network_mode = "bridge" # для EC2
 
   cpu    = 256
   memory = 512
@@ -164,4 +169,6 @@ module "ecs_service" {
 
   subnet_ids         = module.vpc.public_subnets
   security_group_ids = [module.ecs_sg.security_group_id]
+
+  desired_count = 1
 }
